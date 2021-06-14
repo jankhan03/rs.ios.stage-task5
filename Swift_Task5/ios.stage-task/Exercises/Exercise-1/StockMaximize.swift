0deb69b8cd3bf6f622035afbac8616f6032eb347
Yan <jankhan03@mail.ru>
2021-06-14 22:42:09 +0300

    ver1 no3

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,37 @@
 import Foundation
 
 class StockMaximize {
-
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        if (prices.isEmpty || prices.count == 1) { return 0 }
+        var arr = prices
+        arr.append(0)
+        var result = 0
+        result = toDo(prices: &arr, count: arr.count - 1)
+        return result
+    }
+    
+    
+    func toDo(prices: inout [Int], count: Int) -> Int {
+        var max = 0
+        var end = 0
+        var n = 0
+        var m = 0
+        for val in prices
+        {
+            if (n < count && val >= 0)
+            {
+                max = val
+                while (val <= prices[n + m] && max <= prices[n + m])
+                {
+                    max = prices[n + m]
+                    m += 1
+                }
+                end += max - val
+            }
+            n += 1;
+            m = 0;
+        }
+        return end
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -1,8 +1,35 @@
 import UIKit
 
 class MessageDecryptor: NSObject {
-    
+
     func decryptMessage(_ message: String) -> String {
+        if (message.count <= 1 && message.count >= 60) {
+            return "nil"
+        }
+        var unshifr = message
+        while let ourRang = unshifr.range(of: #"[0-9]+\[[a-z]+\]"#, options: .regularExpression){
+            var index = ourRang.lowerBound
+            var couner = ""
+            var str = ""
+
+            while index != ourRang.upperBound {
+                if unshifr[index].isNumber {
+                    couner.append(unshifr[index])
+                } else if unshifr[index].isLetter {
+                    str.append(unshifr[index])
+                }
+                index = unshifr.index(after: index)
+            }
+            let count = Int(couner) ?? 0
+            if count < 1 || count > 300  {
+                return ""
+            }
+            unshifr.removeSubrange(ourRang)
+            unshifr.insert(contentsOf: String.init(repeating: str, count: count), at: ourRang.lowerBound)
+        }
+        if let range = unshifr.range(of: #"[a-z]+"#, options: .regularExpression) {
+            return String(unshifr[range.lowerBound ... unshifr.index(before: (range.upperBound))])
+        }
         return ""
     }
 }
